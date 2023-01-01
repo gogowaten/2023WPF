a66cc47b403118b5c4a0f65e24ce09b213d8b17f
waten <gogowaten@yahoo.co.jp>
2023-01-01 11:47:26 +0900

    202301011147

--- 20230101_Z/20230101_Z/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ 20230101_Z/20230101_Z/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace _20230101_Z {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/20230101_Z;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/20230101_Z;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
