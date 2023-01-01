23ce2da34e4a8257c4470283eeb3dba53b6def45
waten <gogowaten@yahoo.co.jp>
2023-01-01 15:16:19 +0900

    202301011516

--- 20230101_ZUpDown/20230101_ZUpDown/App.xaml.cs
+++ 20230101_ZUpDown/20230101_ZUpDown/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace _20230101_Z
+namespace _20230101_ZUpDown
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- 20230101_ZUpDown/20230101_ZUpDown/MainWindow.xaml.cs
+++ 20230101_ZUpDown/20230101_ZUpDown/MainWindow.xaml.cs
@@ -12,8 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-//
-namespace _20230101_Z
+
+namespace _20230101_ZUpDown
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,7 +23,6 @@ namespace _20230101_Z
         public MainWindow()
         {
             InitializeComponent();
-             
         }
     }
 }
