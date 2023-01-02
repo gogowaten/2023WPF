4dbc4b66b7927c2f50329af11fbc42293316649e
waten <gogowaten@yahoo.co.jp>
2023-01-02 14:00:49 +0900

    202301021400

--- 20230101_ElementToPngFile/20230101_ElementToPngFile/MainWindow.xaml.cs
+++ 20230101_ElementToPngFile/20230101_ElementToPngFile/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 
 namespace _20230101_ElementToPngFile
@@ -27,6 +17,7 @@ namespace _20230101_ElementToPngFile
         /// 要素の座標が0,0以外だった場合はできない。
         /// 回転などの変換がされていた場合もできない。
         /// ActualWidthとActualHeightに数値が入っていない場合もできない。
+        /// スクロールバーなどで全体が表示されていないときもでいない。
         /// </summary>
         /// <param name="element"></param>
         /// <returns></returns>
@@ -44,28 +35,25 @@ namespace _20230101_ElementToPngFile
         /// <summary>
         /// Canvas上に配置された要素をBitmap画像に変換する。
         /// 要素の座標が0,0以外、回転などの変換がされていても正しく変換する。
+        /// ただし、ActualWidthとActualHeightに数値が入っていない要素の場合はできない。
         /// </summary>
         /// <param name="element">対象要素</param>
         /// <param name="parentCanvas">対象要素の親Canvas</param>
         /// <returns></returns>
         private BitmapSource MakeBitmap(FrameworkElement element, Canvas parentCanvas)
         {
+            //対象要素がピッタリ収まるRect取得
             Rect bounds = element.TransformToVisual(parentCanvas)
-                                 .TransformBounds(new Rect(0,
-                                                           0,
-                                                           element.ActualWidth,
-                                                           element.ActualHeight));
+                                 .TransformBounds(
+                new Rect(0, 0, element.ActualWidth, element.ActualHeight));
             VisualBrush brush = new(element) { Stretch = Stretch.None };
             DrawingVisual dv = new();
             using (var context = dv.RenderOpen())
             {
                 context.DrawRectangle(brush, null, new Rect(bounds.Size));
             }
-            RenderTargetBitmap bitmap = new((int)bounds.Width,
-                                            (int)bounds.Height,
-                                            96,
-                                            96,
-                                            PixelFormats.Pbgra32);
+            RenderTargetBitmap bitmap
+                = new((int)bounds.Width, (int)bounds.Height, 96, 96, PixelFormats.Pbgra32);
             bitmap.Render(dv);
             return bitmap;
         }
@@ -73,7 +61,7 @@ namespace _20230101_ElementToPngFile
         /// <summary>
         /// BitmapSourceをpngファイルにする
         /// </summary>
-        /// <param name="bitmap"></param>
+        /// <param name="bitmap">保存する画像</param>
         /// <param name="filePath">保存先パス(拡張子も付けたフルパス)</param>
         private void SaveBitmapToPng(BitmapSource bitmap, string filePath)
         {
@@ -86,14 +74,20 @@ namespace _20230101_ElementToPngFile
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {   
-            SaveBitmapToPng(MakeBitmap(MyRectangle2, MyCanvas), "E:elementImage.png");
+        {
+            SaveBitmapToPng(MakeBitmap(MyRectangle2, MyCanvas), "E:element.png");
             //SaveBitmapToPng(MakeBitmap(MyRectangle1, MyCanvas), "E:elementImage.png");
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            SaveBitmapToPng(MakeBitmap(MyCanvas), "E:AllElementImage.png");
+            SaveBitmapToPng(MakeBitmap(MyCanvas, MyCanvas), "E:Canvas.png");
+            //SaveBitmapToPng(MakeBitmap(MyCanvas), "E:Canvas.png");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SaveBitmapToPng(MakeBitmap(MyCanvas2, MyCanvas), "E:Canvas2.png");
         }
     }
 }
