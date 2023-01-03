f9f672e1f3f4dd2655a480ad01456d6b7e3276ee
waten <gogowaten@yahoo.co.jp>
2023-01-03 22:46:11 +0900

    202301032246

--- 20230102_TextBlockToPng/20230102_TextBlockToPng/MainWindow.xaml.cs
+++ 20230102_TextBlockToPng/20230102_TextBlockToPng/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation.Text;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -55,30 +56,26 @@ namespace _20230102_TextBlockToPng
         /// <returns></returns>
         private BitmapSource MakeBitmap(FrameworkElement element, Canvas parentCanvas)
         {
-            var pchild = VisualTreeHelper.GetChild(parentCanvas, 0);
-            var pcount = VisualTreeHelper.GetChildrenCount(parentCanvas);
-            var pcbounds = VisualTreeHelper.GetContentBounds(parentCanvas);
-            var pdebounds = VisualTreeHelper.GetDescendantBounds(parentCanvas);
-            var edraw = VisualTreeHelper.GetDrawing(element);
             //RenderOptions.SetEdgeMode(element, EdgeMode.Aliased);
             var ebounds = VisualTreeHelper.GetDescendantBounds(element);
-            var eedge = VisualTreeHelper.GetEdgeMode(element);
-            var neko = element.ActualWidth;
-            var wid = ebounds.X + ebounds.Width;
-            var epare = VisualTreeHelper.GetParent(element);
+
 
             //対象要素がピッタリ収まるRect取得
             Rect bounds = element.TransformToVisual(parentCanvas)
                                  .TransformBounds(
                 new Rect(0, 0, element.ActualWidth, element.ActualHeight));
-            bounds = VisualTreeHelper.GetDescendantBounds(element);//余白なしのRectになるのでText表示系要素だとそのままでは使えない
-            //bounds.X = -ebounds.X; bounds.Y = -ebounds.Y;
-            VisualBrush brush = new(element) { Stretch = Stretch.None };
+            //余白なしのRectになるのでText表示系要素だとそのままでは使えない
+            Rect bounds2 = VisualTreeHelper.GetDescendantBounds(element);
+
+
             DrawingVisual dv = new();
             using (var context = dv.RenderOpen())
             {
-                context.DrawRectangle(brush, null, new Rect(bounds.X, bounds.Y, bounds.Width, bounds.Height));
-                //context.DrawRectangle(brush, null, new Rect(bounds.Size));
+                VisualBrush brush = new(element) { Stretch = Stretch.None };
+                //brush.AlignmentX = AlignmentX.Left;
+                //brush.AlignmentY = AlignmentY.Top;
+                //context.DrawRectangle(brush, null, new Rect(bounds.X, bounds.Y, bounds.Width, bounds.Height));
+                context.DrawRectangle(brush, null, new Rect(bounds.Size));
             }
             RenderTargetBitmap bitmap
                 = new((int)bounds.Width + 1, (int)bounds.Height + 1, 96, 96, PixelFormats.Pbgra32);
@@ -103,12 +100,15 @@ namespace _20230102_TextBlockToPng
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //SaveBitmapToPng(MakeBitmap(MyCanvas), "E:textresult.png");
-            SaveBitmapToPng(MakeBitmap(MyCanvas, MyCanvas), "E:textresult.png");
-            //SaveBitmapToPng(MakeBitmap(MyCanvas, ParentCanvas), "E:textresult.png");
+            //SaveBitmapToPng(MakeBitmap(MyCanvas, MyCanvas), "E:textresult.png");
+            SaveBitmapToPng(MakeBitmap(MyCanvas, ParentCanvas), "E:textresult.png");
             //SaveBitmapToPng(MakeBitmap(MyInternalGrid, MyCanvas), "E:textresult.png");
             //var can = MyCanvas;
             //var gri = MyInternalGrid;
             //var tex = MyText;
         }
+    }
+    public class ExTextBlock : TextBlock
+    {
     }
 }
