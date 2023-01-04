cabac85d82f6ceea52070ec82af688bdc834251a
waten <gogowaten@yahoo.co.jp>
2023-01-04 17:02:49 +0900

    202301041702

---
+++ 20230104_DataTemplate_DataType/20230104_DataTemplate_DataType/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//【WPF】XAMLでテンプレートを定義とコードで定義は違う。 | 創造的プログラミングと粘土細工
+//http://pro.art55.jp/?eid=1150138
+
+
+namespace _20230104_DataTemplate_DataType
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Item> Items { get; set; } = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Items = new ObservableCollection<Item>
+            {
+                new PathItem { X = 50, Y = 50, Width = 50, Height = 50, Stroke = Brushes.Red, Data = "M0,25L25,50L50,25L25,0Z", },
+                new EllipseItem { X = 200, Y = 50, Width = 50, Height = 100, Fill = Brushes.Green, },
+                new RectangleItem { X = 200, Y = 200, Width = 100, Height = 50, Fill = Brushes.Blue, },
+            };
+            var neko = MyItemsControl.Template;
+            ResourceDictionary myresource = this.Resources;
+            foreach (var item in myresource.Values)
+            {
+                var val = item;//DataTemplate
+                DataTemplate data = (DataTemplate)item;
+                var tree = data.VisualTree;
+            }
+            foreach (var item in myresource.Keys)
+            {
+                var val = item;//DataTemplateKey
+            }
+            ResourceDictionary rd = new();
+            DataTemplate dt = new(typeof(RectangleItem));
+            FrameworkElementFactory factory = new(typeof(Rectangle));
+            factory.SetValue(WidthProperty, new Binding(nameof(RectangleItem.Width)));
+            factory.SetValue(HeightProperty, new Binding(nameof(RectangleItem.Height)));
+            factory.SetValue(Shape.FillProperty, new Binding(nameof(RectangleItem.Fill)));
+            //factory.SetValue(Canvas.LeftProperty, new Binding(nameof(RectangleItem.X)));
+            dt.VisualTree = factory;
+            DataTemplateKey dkey = new(typeof(RectangleItem));
+            myresource.Add(dkey, dt);
+
+
+            //
+            ObservableCollection<Data> myData= new();
+            myData.Add(new DataText("text", 20, 110, 110, 0));
+
+            MyTTGroup.MyData = myData;
+        }
+    }
+
+    public class Item : INotifyPropertyChanged
+    {
+        //public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        #region 通知プロパティ
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void SetProperty<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName] string? name = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private double _x;
+        public double X { get => _x; set => SetProperty(ref _x, value); }
+        #endregion 通知プロパティ
+
+    }
+    public class PathItem : Item
+    {
+        public Brush Stroke { get; set; } = Brushes.Red;
+        public string Data { get; set; } = "";
+    }
+
+    public class EllipseItem : Item
+    {
+        public Brush Fill { get; set; } = Brushes.MediumAquamarine;
+    }
+
+    //public class ImageItem : Item
+    //{
+    //    public ImageSource Source { get; set; }
+    //}
+
+    public class RectangleItem : Item
+    {
+        public Brush Fill { get; set; } = Brushes.Lime;
+    }
+
+    //public class RichTextBoxItem : Item
+    //{
+    //    public string Text { get; set; }
+    //}
+}
---
+++ 20230104_DataTemplate_DataType/20230104_DataTemplate_DataType/TThumb.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Collections.ObjectModel;
+
+namespace _20230104_DataTemplate_DataType
+{
+    public class TThumb : Thumb
+    {
+        public TThumb()
+        {
+            DataTemplate dTemp = new(typeof(Rectangle));
+
+        }
+
+    }
+    public class TTRectangle : TThumb
+    {
+        public RectangleItem MyData { get; set; } = new();
+        public TTRectangle()
+        {
+            DataContext = MyData;
+        }
+    }
+
+    public class TTGroup : Thumb
+    {
+        public ObservableCollection<Data> MyData { get; set; } = new();
+        public TTGroup()
+        {
+            DataContext = this;
+            SetTemplate();
+            SetResource();
+
+            var neko = this.Resources;
+            foreach (var data in neko.Values)
+            {
+
+            }
+
+        }
+        private void SetTemplate()
+        {
+            FrameworkElementFactory fCanvas = new(typeof(Canvas));
+            ItemsPanelTemplate panelTemp = new(fCanvas);
+            FrameworkElementFactory fItems = new(typeof(ItemsControl));
+            fItems.SetValue(ItemsControl.ItemsPanelProperty, panelTemp);
+            fItems.SetValue(ItemsControl.ItemsSourceProperty, new Binding(nameof(MyData)));
+
+            Style ss = new();
+            Setter sst = new(Canvas.LeftProperty, new Binding(nameof(Data.X)));
+            ss.Setters.Add(sst);
+            sst=new(Canvas.TopProperty, new Binding(nameof(Data.Y)));
+            ss.Setters.Add(sst);
+            fItems.SetValue(ItemsControl.ItemContainerStyleProperty, ss);
+
+            this.Template = new() { VisualTree = fItems };
+
+        }
+        private void SetResource()
+        {
+            ResourceDictionary resource = new();
+
+            FrameworkElementFactory factory = new(typeof(Rectangle));
+            factory.SetValue(WidthProperty, new Binding(nameof(Data.Width)));
+            factory.SetValue(HeightProperty, new Binding(nameof(Data.Height)));
+            factory.SetValue(Shape.FillProperty, new Binding(nameof(DataRect.Brush)));
+
+            DataTemplate dt = new(typeof(DataRect));
+            DataTemplateKey dkey = new(typeof(Rectangle));
+            dt.VisualTree = factory;
+            resource.Add(dkey, dt);
+
+            factory = new(typeof(TextBlock));
+            factory.SetValue(TextBlock.FontSizeProperty, new Binding(nameof(DataText.FontSize)));
+            factory.SetValue(TextBlock.TextProperty, new Binding(nameof(DataText.Text)));
+            factory.SetValue(Canvas.LeftProperty, new Binding(nameof(DataText.X)));
+            dt = new(typeof(DataText));
+            dt.VisualTree = factory; ;
+            resource.Add(new DataTemplateKey(typeof(DataText)), dt);
+
+
+            this.Resources = resource;
+        }
+    }
+
+    public class Data
+    {
+        public Data() { }
+        public Data(double x, double y, double z, double width, double height)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Width = width;
+            Height = height;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+    public class DataText : Data
+    {
+        public DataText(string text, double fontSize, double x, double y, double z) : base(x, y, z, double.NaN, double.NaN)
+        {
+            Text = text;
+            FontSize = fontSize;
+        }
+
+        public string Text { get; set; }
+        public double FontSize { get; set; }
+    }
+    public class DataRect : Data
+    {
+        public DataRect(Brush brush, double x, double y, double z, double width, double height) : base(x, y, z, width, height)
+        {
+            Brush = brush;
+        }
+
+        public Brush Brush { get; set; }
+    }
+
+}
