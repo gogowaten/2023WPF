a806556921dc9fff9000a056f83e6d5d4e1e41ce
waten <gogowaten@yahoo.co.jp>
2023-01-14 17:14:51 +0900

    202301141714

--- 20230114_SerializeWithBitmap/20230114_SerializeWithBitmap/MainWindow.xaml.cs
+++ 20230114_SerializeWithBitmap/20230114_SerializeWithBitmap/MainWindow.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Compression;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace _20230114_SaveDataGroupWithBitmap
+namespace _20230114_SerializeWithBitmap
 {
     public partial class MainWindow : Window
     {
@@ -29,53 +18,56 @@ namespace _20230114_SaveDataGroupWithBitmap
         {
             InitializeComponent();
 
-            TTTextBlock tTTextBlock = new(new DataTextBlock() { Text = "TTT", X = 100, Y = 100 });
-            MyCanvas.Children.Add(tTTextBlock);
+            DataImage dataImage1 = new()
+            {
+                X = 150,
+                Y = 10,
+                ImageSource = GetBitmapImage("D:\\ブログ用\\テスト用画像\\NEC_0541_2017_07_21_午後わてん__Matrix4x4_1.png")
+            };
+            //DataImage単体でセーブロード確認
+            SaveToZip(ZIP_FILE_PATH, dataImage1);
+            Data? neko = LoadFromZip(ZIP_FILE_PATH);
 
-            DataTextBlock dataTextBlock = new() { Text = "TTT2", X = 120, Y = 120 };
-            MyRoot.AddItem(dataTextBlock);
+            DataImage dataImage2 = new()
+            {
+                X = 30,
+                Y = 140,
+                ImageSource = GetBitmapImage($"D:\\ブログ用\\テスト用画像\\collection1.png")
+            };
+            DataTextBlock dataTextBlock = new()
+            {
+                X = 120,
+                Y = 120,
+                Text = "マヨネー樹の花の色、常勝不敗の理を顕す"
+            };
 
-            DataImage dataImage1 = new() { ImageSource = GetBitmapImage2("D:\\ブログ用\\テスト用画像\\NEC_0541_2017_07_21_午後わてん__Matrix4x4_1.png"), X = 150, Y = 10 };
-            SaveToZip1(ZIP_FILE_PATH, dataImage1);
-            Data? neko = LoadFromZip1(ZIP_FILE_PATH);
-
-            DataImage dataImage2 = new() { ImageSource = GetBitmapImage2($"D:\\ブログ用\\テスト用画像\\collection1.png"), X = 30, Y = 140 };
-
+            //DataGroupでセーブロード確認
             DataGroup dataGroup = new() { X = 10, Y = 10 };
             dataGroup.Datas.Add(dataImage1);
             dataGroup.Datas.Add(dataTextBlock);
             dataGroup.Datas.Add(dataImage2);
-            SaveToZip1(ZIP_FILE_PATH, dataGroup);
-            Data? inu= LoadFromZip1(ZIP_FILE_PATH);
+            SaveToZip(ZIP_FILE_PATH, dataGroup);
+            Data? inu = LoadFromZip(ZIP_FILE_PATH);
 
-            TTImage MyTTImage = new(dataImage2);
-            MyRoot.AddItem(MyTTImage, dataImage2);
-
-            Serialize2($"E:\\20230113.xml", MyRoot.Data);
-            var dedata = Deserializer<Data>($"E:\\20230113.xml");
-            SaveToZip1($"E:\\20230113.zip", MyRoot.Data);
-            //SaveToZip($"E:\\20230113.zip", MyRoot.Data);
-            //var neko = LoadFromZip($"E:\\20230113.zip");
-            var uma = LoadFromZip1($"E:\\20230113.zip");
         }
 
 
-        private BitmapImage GetBitmapImage2(string path)
+        private BitmapImage GetBitmapImage(string path)
         {
             BitmapImage bitmap = new();
             FileStream stream = File.OpenRead(path);
             {
                 bitmap.BeginInit();
                 bitmap.StreamSource = stream;
-                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                //bitmap.CacheOption = BitmapCacheOption.OnLoad;
                 //bitmap.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
                 bitmap.EndInit();
-                bitmap.Freeze();
+                //bitmap.Freeze();
             }
             return bitmap;
         }
 
-        private void SaveToZip1(string filePath, Data data)
+        private void SaveToZip(string filePath, Data data)
         {
             using FileStream zipStream = File.Create(filePath);
             using (ZipArchive archive = new(zipStream, ZipArchiveMode.Create))
@@ -131,7 +123,7 @@ namespace _20230114_SaveDataGroupWithBitmap
         }
 
 
-        private Data? LoadFromZip1(string filePath)
+        private Data? LoadFromZip(string filePath)
         {
             try
             {
@@ -140,11 +132,13 @@ namespace _20230114_SaveDataGroupWithBitmap
                 ZipArchiveEntry? entry = archive.GetEntry(XML_FILE_NAME);
                 if (entry != null)
                 {
+                    //デシリアライズ
                     using Stream entryStream = entry.Open();
                     DataContractSerializer serializer = new(typeof(Data));
                     using var reader = XmlReader.Create(entryStream);
                     Data? data = (Data?)serializer.ReadObject(reader);
                     if (data is null) return null;
+                    //DataがDataImage型ならzipから画像を取り出して設定
                     Sub(data, archive);
                     return data;
                 }
@@ -171,6 +165,7 @@ namespace _20230114_SaveDataGroupWithBitmap
             }
             void SubSub(DataImage data, ZipArchive archive)
             {
+                //Guidに一致する画像ファイルをデコードしてプロパティに設定
                 ZipArchiveEntry? imageEntry = archive.GetEntry(data.Guid + ".png");
                 if (imageEntry != null)
                 {
@@ -179,7 +174,7 @@ namespace _20230114_SaveDataGroupWithBitmap
                         new(imageStream,
                         BitmapCreateOptions.None,
                         BitmapCacheOption.Default);
-                    data.ImageSource = decoder.Frames[0];
+                    data.ImageSource = decoder.Frames[0];//設定
                 }
             }
         }
@@ -187,7 +182,7 @@ namespace _20230114_SaveDataGroupWithBitmap
 
         #region シリアライズ
 
-        ////それぞれの派生クラス型でシリアライズ
+        ////それぞれのクラス型でシリアライズ
         //private static void Serialize<T>(string filePath, T obj)
         //{
         //    XmlWriterSettings settings = new()
@@ -233,9 +228,5 @@ namespace _20230114_SaveDataGroupWithBitmap
 
 
 
-        private void ButtonTest1_Click(object sender, RoutedEventArgs e)
-        {
-            var neko = MyRoot.Data;
-        }
     }
 }
