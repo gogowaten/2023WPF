9785069f02ce761a3cf11a43b2e74638daaff0ef
waten <gogowaten@yahoo.co.jp>
2023-02-24 15:32:06 +0900

    202302241531

---
+++ 20230224_GeometryAnchorThumbs/20230224_GeometryAnchorThumbs/AnchorThumb.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace _20230224_GeometryAnchorThumbs
+{
+    public class AnchorThumb
+    {
+    }
+    public class TThumb : Thumb
+    {
+
+        public double X
+        {
+            get { return (double)GetValue(XProperty); }
+            set { SetValue(XProperty, value); }
+        }
+        public static readonly DependencyProperty XProperty =
+            DependencyProperty.Register(nameof(X), typeof(double), typeof(TThumb),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double Y
+        {
+            get { return (double)GetValue(YProperty); }
+            set { SetValue(YProperty, value); }
+        }
+        public static readonly DependencyProperty YProperty =
+            DependencyProperty.Register(nameof(Y), typeof(double), typeof(TThumb),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public TThumb(Point vartex) : this()
+        {
+            X = vartex.X;
+            Y = vartex.Y;
+        }
+        public TThumb()
+        {
+            this.Template = MakeTemplate();
+            Canvas.SetLeft(this, 0);
+            Canvas.SetTop(this, 0);
+            this.DataContext = this;
+            this.SetBinding(Canvas.LeftProperty, nameof(X));
+            this.SetBinding(Canvas.TopProperty, nameof(Y));
+            DragDelta += TThumb_DragDelta;
+            Width = 20;
+            Height = 20;
+        }
+
+        private void TThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is TThumb tt)
+            {
+                Canvas.SetLeft(tt, Canvas.GetLeft(tt) + e.HorizontalChange);
+                Canvas.SetTop(tt, Canvas.GetTop(tt) + e.VerticalChange);
+                //tt.X += e.HorizontalChange;
+                //tt.Y += e.VerticalChange;
+            }
+        }
+
+        private ControlTemplate MakeTemplate()
+        {
+            FrameworkElementFactory elementF = new(typeof(Rectangle));
+            elementF.SetValue(Rectangle.FillProperty, Brushes.Transparent);
+            elementF.SetValue(Rectangle.StrokeProperty, Brushes.Black);
+            elementF.SetValue(Rectangle.StrokeDashArrayProperty,
+                new DoubleCollection() { 2.0 });
+            return new ControlTemplate() { VisualTree = elementF };
+        }
+    }
+}
---
+++ 20230224_GeometryAnchorThumbs/20230224_GeometryAnchorThumbs/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20230224_GeometryAnchorThumbs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PointCollection MyPoints { get; set; } = new();
+        //public ObservableCollection<Point> MyPoints { get; set; } = new();
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            AnchorTest();
+        }
+        private void AnchorTest()
+        {
+            MyPoints.Add(new(20, 100));
+            MyPoints.Add(new(100, 10));
+            //foreach (var item in MyPoints)
+            //{
+            //    MyThumbs.Add(new TThumb(item));
+            //}
+
+            MyItemsControl.DataContext = MyPoints;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var neko = MyPoints;
+            //var inu = MyThumbs[0].X;
+        }
+    }
+}
