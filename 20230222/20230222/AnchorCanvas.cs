c701b3942d27002abae49aa40c0aecd0d795856b
waten <gogowaten@yahoo.co.jp>
2023-02-27 10:20:36 +0900

    202302271020

---
+++ 20230222/20230222/AnchorCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace _20230222
+{
+    //:Canvas
+    //  List<AnchorThumb>
+    //  PolyBezierArrowLine2
+    public class AnchorCanvas : Canvas
+    {
+        public List<AnchorThumb> MyThumbs;
+        public PolyBezierArrowLine2 MyPolyBezierArrowLine2 { get; set; }
+        public AnchorCanvas()
+        {
+            MyThumbs = new List<AnchorThumb>();
+
+        }
+    }
+
+}
