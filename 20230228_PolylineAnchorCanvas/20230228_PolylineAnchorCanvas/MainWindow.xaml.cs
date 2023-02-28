796054de2a4772ea5d1097f0caa05a2c8c75031a
waten <gogowaten@yahoo.co.jp>
2023-02-28 16:11:52 +0900

    202302281611

---
+++ 20230228_PolylineAnchorCanvas/20230228_PolylineAnchorCanvas/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Polyline表示、Polylineのアンカー点表示非表示、アンカー点のマウス移動
+//アンカー点の追加と削除は右クリックメニュー
+
+//アンカー点がマイナス座標になった場合の処理がまだ
+namespace _20230228_PolylineAnchorCanvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyPolylineCanvas.MyAnchorVisible != Visibility.Visible)
+            {
+                MyPolylineCanvas.MyAnchorVisible = Visibility.Visible;
+            }
+            else
+            {
+                MyPolylineCanvas.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
---
+++ 20230228_PolylineAnchorCanvas/20230228_PolylineAnchorCanvas/PolylinCanvas.cs
@@ -0,0 +1,149 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+//
+namespace _20230228_PolylineAnchorCanvas
+{
+    class PolylinCanvas : Canvas
+    {
+        public PointCollection MyPoints
+        {
+            get { return (PointCollection)GetValue(MyPointsProperty); }
+            set { SetValue(MyPointsProperty, value); }
+        }
+        public static readonly DependencyProperty MyPointsProperty =
+            DependencyProperty.Register(nameof(MyPoints), typeof(PointCollection), typeof(PolylinCanvas),
+                new FrameworkPropertyMetadata(new PointCollection(),
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public Visibility MyAnchorVisible
+        {
+            get { return (Visibility)GetValue(MyAnchorVisibleProperty); }
+            set { SetValue(MyAnchorVisibleProperty, value); }
+        }
+        public static readonly DependencyProperty MyAnchorVisibleProperty =
+            DependencyProperty.Register(nameof(MyAnchorVisible), typeof(Visibility), typeof(PolylinCanvas),
+                new FrameworkPropertyMetadata(Visibility.Collapsed,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public ObservableCollection<AnchorThumb> MyAnchorThumbs { get; set; } = new();
+
+        public Polyline MyShape { get; set; }
+        public ContextMenu MyAnchorMenu { get; set; } = new();
+        public ContextMenu MyMenu { get; set; } = new();
+        public AnchorThumb? MyCurrentAnchorThumb { get; set; }
+        public int MyDragPointIndex;
+        public Point MyClickedPoint;
+
+        public PolylinCanvas()
+        {
+            this.Background = Brushes.Transparent;
+            MyShape = new Polyline();
+            Children.Add(MyShape);
+            MyShape.Stroke = Brushes.MediumAquamarine;
+            MyShape.StrokeThickness = 20;
+
+            Loaded += PolylinCanvas_Loaded;
+
+            SetBinding(WidthProperty, new Binding() { Source = MyShape, Path = new PropertyPath(ActualWidthProperty) });
+            SetBinding(HeightProperty, new Binding() { Source = MyShape, Path = new PropertyPath(ActualHeightProperty) });
+
+            MenuItem item = new() { Header = "削除" };
+            item.Click += (o, e) => { RemovePoint(MyCurrentAnchorThumb); };
+            MyAnchorMenu.Items.Add(item);
+
+            this.ContextMenu = MyMenu;
+            item = new() { Header = "ここに追加" };
+            item.Click += (o, e) => { AddPoint(MyClickedPoint); };
+            MyMenu.Items.Add(item);
+        }
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            MyClickedPoint = Mouse.GetPosition(this);
+        }
+
+
+        private void PolylinCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            MyShape.Points = MyPoints;
+            foreach (var item in MyPoints)
+            {
+                AddAnchorThumb(item);
+            }
+        }
+
+        public void AddPoint(Point point)
+        {
+            InsertPoint(point, MyPoints.Count);
+        }
+        public void RemovePoint(int pointIndex)
+        {
+            if (pointIndex < 0) { return; }
+            MyPoints.RemoveAt(pointIndex);
+            AnchorThumb thumb = MyAnchorThumbs[pointIndex];
+            MyAnchorThumbs.Remove(thumb);
+            Children.Remove(thumb);
+            MyCurrentAnchorThumb = null;
+        }
+        public void RemovePoint(AnchorThumb? thumb)
+        {
+            if (thumb == null) { return; }
+            RemovePoint(MyDragPointIndex);
+        }
+
+        public void InsertPoint(Point point, int i)
+        {
+            MyPoints.Insert(i, point);
+            InsertAnchorThumb(point, i);
+        }
+        private void AddAnchorThumb(Point point)
+        {
+            InsertAnchorThumb(point, MyAnchorThumbs.Count);
+        }
+        private void InsertAnchorThumb(Point point, int i)
+        {
+            AnchorThumb thumb = new(point);
+            MyAnchorThumbs.Insert(i, thumb);
+            Children.Add(thumb);
+            thumb.DragDelta += Thumb_DragDelta;
+            thumb.SetBinding(VisibilityProperty, new Binding()
+            {
+                Source = this,
+                Path = new PropertyPath(MyAnchorVisibleProperty)
+            });
+            thumb.ContextMenu = MyAnchorMenu;
+            thumb.PreviewMouseDown += Thumb_PreviewMouseDown;
+        }
+
+        private void Thumb_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is AnchorThumb thumb)
+            {
+                MyCurrentAnchorThumb = thumb;
+                MyDragPointIndex = MyAnchorThumbs.IndexOf(thumb);
+            }
+            else { MyDragPointIndex = -1; }
+        }
+
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is AnchorThumb thumb)
+            {
+                thumb.X += e.HorizontalChange;
+                thumb.Y += e.VerticalChange;
+                MyPoints[MyDragPointIndex] = new Point(thumb.X, thumb.Y);
+            }
+        }
+    }
+}
