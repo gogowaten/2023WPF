f3c2b2102aeb1d0a6e0c8bb5d61144d66e2ef900
waten <gogowaten@yahoo.co.jp>
2023-03-03 22:56:48 +0900

    202303032256

---
+++ 20230303/20230303/Ador.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+//embellishとadornとdecorateの語感の違い... - Yahoo!知恵袋
+//https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q1023045950
+
+//adorn：飾る、装飾する、アドーン
+//adorner：装飾者、アドーナー、アドナー
+//装飾：decorate、装飾者：decorator
+//Adol Christhin
+
+//装飾の概要 - WPF .NET Framework | Microsoft Learn
+//https://learn.microsoft.com/ja-jp/dotnet/desktop/wpf/controls/adorners-overview?view=netframeworkdesktop-4.8
+//WPF Adorners Part 1 – What are adorners
+//https://www.nbdtech.com/Blog/archive/2010/06/21/wpf-adorners-part-1-ndash-what-are-adorners.aspx
+
+namespace _20230303
+{
+    class Ador : Adorner
+    {
+        public Ador(UIElement adornedElement) : base(adornedElement)
+        {
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            //base.OnRender(drawingContext);
+
+            SolidColorBrush brush = new(Color.FromArgb(50, 200, 0, 0));
+            Pen pen = new Pen(Brushes.Red, 1.0);
+            double radius = 10.0;
+            Rect rect = new Rect(this.AdornedElement.DesiredSize);
+            drawingContext.DrawEllipse(brush, pen, rect.TopLeft, radius, radius);
+            drawingContext.DrawEllipse(brush, pen, rect.TopRight, radius, radius);
+            drawingContext.DrawEllipse(brush, pen, rect.BottomLeft, radius, radius);
+            drawingContext.DrawEllipse(brush, pen, rect.BottomRight, radius, radius);
+        }
+    }
+}
---
+++ 20230303/20230303/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20230303
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            AdornerLayer.GetAdornerLayer(MyTextBox).Add(new Ador(MyTextBox));
+            AdornerLayer.GetAdornerLayer(MyBorder).Add(new Ador(MyBorder));
+            AdornerLayer.GetAdornerLayer(MyButton).Add(new Ador(MyButton));
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rect = MyBorder.RenderSize;
+            var conrect = VisualTreeHelper.GetContentBounds(MyBorder);
+        }
+    }
+}
