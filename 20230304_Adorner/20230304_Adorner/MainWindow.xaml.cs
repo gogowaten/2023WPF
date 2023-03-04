4d2e2a247263ed1760ed2196b139367908e4f3db
waten <gogowaten@yahoo.co.jp>
2023-03-04 18:06:54 +0900

    202303041806

---
+++ 20230304_Adorner/20230304_Adorner/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20230304_Adorner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+            
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (AdornerLayer.GetAdornerLayer(MyTextBox) is AdornerLayer layer)
+            {
+                layer.Add(new ResizeAdorn(MyTextBox));
+            }
+        }
+    }
+}
---
+++ 20230304_Adorner/20230304_Adorner/ResizeAdorn.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace _20230304_Adorner
+{
+    internal class ResizeAdorn : Adorner
+    {
+        private Thumb MyThumb;
+        private VisualCollection MyVisualChild;
+        public ResizeAdorn(UIElement adornedElement) : base(adornedElement)
+        {
+            MyVisualChild = new VisualCollection(this);
+            MyThumb = new()
+            {
+                Width = 20,
+                Height = 20,
+                Opacity = 0.2,
+                Background = Brushes.Blue,
+            };
+            MyVisualChild.Add(MyThumb);
+            MyThumb.DragDelta += MyThumb_DragDelta;
+        }
+
+        private void MyThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (AdornedElement is FrameworkElement target)
+            {
+                EnforceSize(target);
+                target.Width = Math.Max(target.Width + e.HorizontalChange, MyThumb.DesiredSize.Width);
+                target.Height = Math.Max(target.Height + e.VerticalChange, MyThumb.DesiredSize.Height);
+            }
+        }
+        private void EnforceSize(FrameworkElement elem)
+        {
+            if (elem.Width.Equals(double.NaN)) { elem.Width = elem.DesiredSize.Width; }
+            if (elem.Height.Equals(double.NaN)) { elem.Height = elem.DesiredSize.Height; }
+            if (elem.Parent is FrameworkElement parent)
+            {
+                elem.MaxHeight = parent.ActualHeight;
+                elem.MaxWidth = parent.ActualWidth;
+            }
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            //return base.ArrangeOverride(finalSize);
+            base.ArrangeOverride(finalSize);
+            double targetW = AdornedElement.DesiredSize.Width;
+            double targetH = AdornedElement.DesiredSize.Height;
+            double w = DesiredSize.Width;
+            double h = DesiredSize.Height;
+            MyThumb.Arrange(new Rect(targetW - w / 2, targetH - h / 2, w, h));
+            return finalSize;
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                //return base.VisualChildrenCount;
+                return MyVisualChild.Count;
+            }
+        }
+        protected override Visual GetVisualChild(int index)
+        {
+            //return base.GetVisualChild(index);
+            return MyVisualChild[index];
+        }
+    }
+}
