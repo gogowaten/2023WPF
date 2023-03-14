f5277ffcafaebd23da023769c7b33adb8e95d68d
waten <gogowaten@yahoo.co.jp>
2023-03-14 18:33:03 +0900

    202303141832

---
+++ 20230314_Event/20230314_Event/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20230314_Event
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Sample sample = new Sample();
+            sample.Time += new EventHandler(this.Sample_Time);
+            sample.Start();
+        }
+
+        private void Sample_Time(object sender, EventArgs e)
+        {
+            MessageBox.Show("ok");
+        }
+    }
+}
---
+++ 20230314_Event/20230314_Event/Sample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _20230314_Event
+{
+    class Sample
+    {
+        public event EventHandler Time;
+        public void Start()
+        {
+            Thread.Sleep(1000);
+            if(Time != null)
+            {
+                Time(this, EventArgs.Empty);
+            }
+        }
+
+    }
+  
+
+
+
+}
