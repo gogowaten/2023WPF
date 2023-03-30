95a6a822cc03c1992c23e55e81677080940a9f28
waten <gogowaten@yahoo.co.jp>
2023-03-31 00:58:31 +0900

    202303310058

--- 20230330_BezierCanvasThumb/20230330_BezierCanvasThumb/MainWindow.xaml.cs
+++ 20230330_BezierCanvasThumb/20230330_BezierCanvasThumb/MainWindow.xaml.cs
@@ -13,6 +13,28 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//できた
+//移動と頂点移動できるベジェ曲線のThumb
+
+//TThumb、Thumb継承、TemplateをCanvasに変更、このCanvasにBezierを追加表示している
+
+//Bezier、Shape継承、AdornerにMyAdornerを追加している、これで頂点Thumbを表示している
+
+//MyAdorner、Adorner継承、ベジェ曲線の頂点をThumbで表示している、ドラッグ移動で頂点座標も更新
+
+//TThumb(マウスドラッグ移動可能、頂点Thumb移動終了時にイベント通知を受け取り座標修正する)
+//  ┗Canvas(Template)
+//      ┗Bezier
+//          ┗MyAdorner
+//              ┗VisualCollection
+//                  ┗Canvas
+//                      ┗Thumb(頂点表示用、マウスドラッグ移動可能、移動終了時にそれをイベントで通知)
+
+//TThumbのTemplateをBezierじゃなくてCanvasにしているのは、もしBezierにすると
+//Bezierの座標を指定してもCanvasがないとそれが反映されないから
+
+//
+
 namespace _20230330_BezierCanvasThumb
 {
     /// <summary>
