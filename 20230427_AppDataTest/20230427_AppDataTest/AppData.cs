40580420c4028cb3bcef3c45031bb3e9b578b263
waten <gogowaten@yahoo.co.jp>
2023-04-27 18:21:35 +0900

    202304271821

---
+++ 20230427_AppDataTest/20230427_AppDataTest/AppData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _20230427_AppDataTest
+{
+    [DataContract]
+    public class AppDatas : DependencyObject
+    {
+        [DataMember] public ObservableCollection<AppData> Datas { get; set; }
+        public AppDatas() {
+            Datas = new ObservableCollection<AppData>();
+            for (int i = 0; i < 10; i++)
+            {
+                Datas.Add(new AppData());
+            }
+        }
+    }
+
+
+    [DataContract]
+    public class AppData : DependencyObject, IExtensibleDataObject
+    {
+        public AppData()
+        {
+
+        }
+
+        public ExtensionDataObject? ExtensionData { get; set; }// => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Name { get; set; } = "nemo";
+
+        [DataMember]
+        public double AppLeft
+        {
+            get { return (double)GetValue(AppLeftProperty); }
+            set { SetValue(AppLeftProperty, value); }
+        }
+        public static readonly DependencyProperty AppLeftProperty =
+            DependencyProperty.Register(nameof(AppLeft), typeof(double), typeof(AppData),
+                new FrameworkPropertyMetadata(0.0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+
+    }
+
+}
---
+++ 20230427_AppDataTest/20230427_AppDataTest/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace _20230427_AppDataTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public AppDatas MyAppDatas { get; set; }
+        public AppData MyAppData { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyAppDatas = new();
+            MyAppData = new() { AppLeft = 200 };
+            SetMyBindings();
+            SetMyComboBox();
+        }
+
+        private void SetMyComboBox()
+        {
+            MyComboBoxAppDatas.ItemsSource = MyAppDatas.Datas;
+            
+        }
+        private void SetMyBindings()
+        {
+            DataContext = MyAppDatas.Datas[0];
+            SetBinding(LeftProperty, new Binding() { Path = new PropertyPath(AppData.AppLeftProperty) });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = MyAppData;
+            SetBinding(LeftProperty, new Binding() { Path = new PropertyPath(AppData.AppLeftProperty) });
+        }
+
+        private void Button_Click_Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void DataSave(string fileName,AppDatas data)
+        {
+            XmlWriterSettings settings = new()
+            {
+                Encoding = new UTF8Encoding(false),
+                Indent = true,
+                NewLineOnAttributes = false,
+                ConformanceLevel = ConformanceLevel.Fragment
+            };
+
+        }
+    }
+}
