686ee067dd7e4fb9349de2160ced6d93af323a5b
waten <gogowaten@yahoo.co.jp>
2023-04-30 23:54:22 +0900

    202304302354

--- 20230428_AppDataDp/20230428_AppDataDp/AppData.cs
+++ 20230428_AppDataDp/20230428_AppDataDp/AppData.cs
@@ -1,29 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace _20230428_AppDataDp
 {
-    class AppData:DependencyObject
+
+    public class AppData : DependencyObject, IExtensibleDataObject//, INotifyPropertyChanged
     {
         public AppData() { }
 
+        public int Nume { get; set; } = 1111;
 
+        [DataMember]
         public double Left
         {
             get { return (double)GetValue(LeftProperty); }
             set { SetValue(LeftProperty, value); }
         }
+
+        [DataMember] public ExtensionDataObject? ExtensionData { get; set; }
+
         public static readonly DependencyProperty LeftProperty =
             DependencyProperty.Register(nameof(Left), typeof(double), typeof(AppData),
-                new FrameworkPropertyMetadata(0.0,
+                new FrameworkPropertyMetadata(100.0,
                     FrameworkPropertyMetadataOptions.AffectsRender |
                     FrameworkPropertyMetadataOptions.AffectsMeasure |
                     FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
+        [DataMember]
         public string Memo
         {
             get { return (string)GetValue(MemoProperty); }
@@ -31,10 +40,22 @@ namespace _20230428_AppDataDp
         }
         public static readonly DependencyProperty MemoProperty =
             DependencyProperty.Register(nameof(Memo), typeof(string), typeof(AppData),
-                new FrameworkPropertyMetadata("None",
+                new FrameworkPropertyMetadata("ないです",
                     FrameworkPropertyMetadataOptions.AffectsRender |
                     FrameworkPropertyMetadataOptions.AffectsMeasure |
                     FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        //public event PropertyChangedEventHandler? PropertyChanged;
+
+        //protected void SetProperty<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName] string? name = null)
+        //{
+        //    if (EqualityComparer<T>.Default.Equals(field, value)) return;
+        //    field = value;
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        //}
+
+        //[DataMember] private string _notify = "通知ないです";
+        //public string Notify { get => _notify; set => SetProperty(ref _notify, value); }
 
     }
 }
--- 20230428_AppDataDp/20230428_AppDataDp/MainWindow.xaml.cs
+++ 20230428_AppDataDp/20230428_AppDataDp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace _20230428_AppDataDp
 {
@@ -20,10 +22,88 @@ namespace _20230428_AppDataDp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private AppData MyAppData { get; set; } = new();
+        private AppData MyAppData { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+
+            //MyAppData = new AppData();
+            MyAppData = LoadData<AppData>($"E://20230428_AppDataDpTest.xml");
+            MyBindings();
+        }
+        private void MyBindings()
+        {
+            SetBinding(LeftProperty, new Binding() { Source = MyAppData, Path = new PropertyPath(AppData.LeftProperty), Mode = BindingMode.TwoWay });
+
+            //Target用
+            MyTextBoxTarget.SetBinding(TextBox.TextProperty, new Binding() { Source = MyAppData, Path = new PropertyPath(AppData.MemoProperty), Mode = BindingMode.TwoWay });
+
+            //以下は確認用
+            MyTextBlock0.SetBinding(TextBlock.TextProperty, new Binding() { Source = MyAppData, Path = new PropertyPath(AppData.LeftProperty) });
+            MyTextBlock1.SetBinding(TextBlock.TextProperty, new Binding() { Source = MyAppData, Path = new PropertyPath(AppData.MemoProperty) });
+
+        }
+
+        #region シリアライズ
+
+        private void SaveData<T>(string fileName, T data)
+        {
+            XmlWriterSettings settings = new()
+            {
+                Encoding = new UTF8Encoding(false),
+                Indent = true,
+                NewLineOnAttributes = false,
+                ConformanceLevel = ConformanceLevel.Fragment
+            };
+            XmlWriter writer;
+            DataContractSerializer serializer = new(typeof(T));
+            using (writer = XmlWriter.Create(fileName, settings))
+            {
+                try { serializer.WriteObject(writer, data); }
+                catch (Exception) { throw; }
+            }
+        }
+
+        private T LoadData<T>(string fileName)
+        {
+            DataContractSerializer serializer = new(typeof(T));
+            try
+            {
+                using XmlReader reader = XmlReader.Create(fileName);
+                if (serializer.ReadObject(reader) is T t)
+                {
+                    return t;
+                }
+                else throw new NullReferenceException();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+        #endregion シリアライズ
+
+        private void MyButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            string fileName = $"E://20230428_AppDataDpTest.xml";
+            SaveData<AppData>(fileName, MyAppData);
+        }
+
+        private void MyButtonLoad_Click(object sender, RoutedEventArgs e)
+        {
+            string fileName = $"E://20230428_AppDataDpTest.xml";
+            AppData data = LoadData<AppData>(fileName);
+            ChangeData(data);
+        }
+        private void ChangeData(AppData data)
+        {
+            MyAppData = data;
+            MyBindings();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var neko = MyAppData;
         }
     }
 }
