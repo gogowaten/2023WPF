cf338f0effcdd5f46a884b2dde8b379eb07dc0bf
waten <gogowaten@yahoo.co.jp>
2023-05-10 12:55:40 +0900

    202305101255

---
+++ 20230510_PointCollectionBinding/20230510_PointCollectionBinding/GeoLine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace _20230510_PointCollectionBinding
+{
+    [ContentProperty(nameof(Anchors))]
+    class GeoLine : Shape
+    {
+
+        public PointCollection Anchors
+        {
+            get { return (PointCollection)GetValue(AnchorsProperty); }
+            set { SetValue(AnchorsProperty, value); }
+        }
+        public static readonly DependencyProperty AnchorsProperty =
+            DependencyProperty.Register(nameof(Anchors), typeof(PointCollection), typeof(GeoLine),
+                new FrameworkPropertyMetadata(new PointCollection() { new Point(0, 0), new Point(100, 100) },
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                StreamGeometry geometry = new();
+                using (var context = geometry.Open())
+                {
+                    context.BeginFigure(Anchors[0], false, false);
+                    context.PolyLineTo(Anchors.Skip(1).ToList(), true, false);
+                }
+                geometry.Freeze();
+                return geometry;
+            }
+        }
+    }
+}
--- 20230510_PointCollectionBinding/20230510_PointCollectionBinding/MainWindow.xaml.cs
+++ 20230510_PointCollectionBinding/20230510_PointCollectionBinding/MainWindow.xaml.cs
@@ -13,6 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//PointCollection同士の双方向Binding
+//問題ない
+//Pointの変更で相手側も変更される
+//Collection自体を変更した場合も同じく変更される
+//注意したいのはBinding時、Sourceの値が優先される
+
 namespace _20230510_PointCollectionBinding
 {
     /// <summary>
@@ -20,9 +26,57 @@ namespace _20230510_PointCollectionBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public PointCollection MyProperty
+        {
+            get { return (PointCollection)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register(nameof(MyProperty), typeof(PointCollection), typeof(MainWindow),
+                new FrameworkPropertyMetadata(null,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
         public MainWindow()
         {
             InitializeComponent();
+            //MyProperty -> GeoLineにする場合
+            //SetBinding(MyPropertyProperty, new Binding() { Source = MyGeoLine1, Path = new PropertyPath(GeoLine.AnchorsProperty), Mode = BindingMode.TwoWay });
+
+            //GeoLine -> MyPropertyにする場合
+            if (MyProperty == null || MyProperty.Count < 2)
+            {
+                MyProperty = MyGeoLine1.Anchors;
+            }
+            MyGeoLine1.SetBinding(GeoLine.AnchorsProperty, new Binding() { Source = this, Path = new PropertyPath(MyPropertyProperty), Mode = BindingMode.TwoWay });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyGeoLine1.Anchors.Add(new Point(0, 100));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var neko = MyProperty;
+            var inu = MyGeoLine1.Anchors;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MyProperty.Add(new Point(50, 70));
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MyProperty = new PointCollection() { new Point(21, 200), new Point(100, 100) };
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MyGeoLine1.Anchors = new PointCollection() { new Point(21, 200), new Point(100, 100) };
         }
     }
 }
