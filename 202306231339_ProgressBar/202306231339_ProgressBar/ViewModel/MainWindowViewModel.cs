802c35901ce0abf8bf7365d30ce3789383f68c8a
waten <gogowaten@yahoo.co.jp>
2023-06-24 15:40:50 +0900

    202306241540

--- 202306231339_ProgressBar/202306231339_ProgressBar/ViewModel/MainWindowViewModel.cs
+++ 202306231339_ProgressBar/202306231339_ProgressBar/ViewModel/MainWindowViewModel.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 //【WPF】コマンドの使い方（ICommandインターフェース）
 //https://marunaka-blog.com/wpf-icommand/5279/
@@ -12,10 +14,151 @@ using System.Windows;
 namespace _202306231339_ProgressBar.ViewModel
 {
 
-    public class MainWindowViwModel1
+    public class MainWindowViewModel3 : ViewModelBase
+    {
+        private CancellationTokenSource CancellationTokenSource;
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                if (isBusy != value)
+                {
+                    isBusy = value;
+                    OnPropertyChanged(nameof(IsBusy));
+                    ButtonCommand.DelegateCanExecute();
+                    CancelCommand.DelegateCanExecute();
+                }
+            }
+        }
+
+        private int prgValue;
+
+        public int PrgValue
+        {
+            get { return prgValue; }
+            set { prgValue = value; OnPropertyChanged(nameof(PrgValue)); }
+        }
+
+        private string message = "";
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged(nameof(Message)); }
+        }
+
+        public DelegateCommand ButtonCommand { get; set; }
+        public DelegateCommand CancelCommand { get; set; }
+
+        public MainWindowViewModel3()
+        {
+            CancellationTokenSource = new();
+            ButtonCommand = new(async () => await ExecuteAsync(), CanExecute);
+            CancelCommand = new(() => CancellationTokenSource.Cancel(), CanCancel);
+        }
+
+        private async Task ExecuteAsync()
+        {
+            IsBusy = true;
+            await WorkTask(CancellationTokenSource.Token);
+            CancellationTokenSource = new();
+            IsBusy = false;
+        }
+
+      
+        private async Task WorkTask(CancellationToken token)
+        {
+            await Task.Run(async () =>
+            {
+                Message = "処理中";
+                for (PrgValue = 0; PrgValue < 100; PrgValue++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Message = "処理中止";
+                        return;
+                    }
+                    //Thread.Sleep(100);
+                    await Task.Delay(10);
+                }
+                Message = "処理完了";
+            }, token);
+        }
+
+        private bool CanExecute() => !IsBusy;
+        private bool CanCancel() => IsBusy;
+
+    }
+
+    /// <summary>
+    /// 足し算引き算、ラジオボタン
+    /// </summary>
+    public class MainWindowViewModel2 : ViewModelBase
+    {
+
+        private double firstValue;
+        public double FirstValue
+        {
+            get => firstValue; set
+            {
+                firstValue = value;
+                PerformOperation(null);
+            }
+        }
+
+        private double secondValue;
+        public double SecondValue { get => secondValue; set { secondValue = value; PerformOperation(null); } }
+
+        private double resultValue;
+        public double ResultValue { get => resultValue; set { resultValue = value; OnPropertyChanged(nameof(ResultValue)); } }
+        //public double ResultValue { get => resultValue; set { resultValue = value; SetProperty(ref resultValue, value); } }
+
+        private string operation = "+";
+        public string Operation { get => operation; set { operation = value; OnPropertyChanged(nameof(Operation)); } }
+
+        private string operationGiven = "Add";
+
+
+        public DelegateCommand<string> OperationCommand { get; }
+
+        public MainWindowViewModel2()
+        {
+            OperationCommand = new DelegateCommand<string>(x => PerformOperation(x));
+        }
+
+        private void PerformOperation(string? param)
+        {
+            if (param != null) { operationGiven = param; }
+            switch (operationGiven)
+            {
+                case "Add": Add(); break;
+                case "Minus": Substract(); break;
+            }
+        }
+
+        private void Add()
+        {
+            Operation = "+";
+            ResultValue = firstValue + secondValue;
+        }
+
+        private void Substract()
+        {
+            Operation = "-";
+            ResultValue = firstValue - secondValue;
+        }
+
+    }
+
+
+    public class MainWindowViewModel1
     {
         private string showText = "";
-        public string ShowText {
+        public string ShowText
+        {
             get { return showText; }
             set
             {
@@ -25,7 +168,7 @@ namespace _202306231339_ProgressBar.ViewModel
         }
 
         public DelegateCommand ShowCommand { get; }
-        public MainWindowViwModel1()
+        public MainWindowViewModel1()
         {
             ShowCommand = new DelegateCommand(execute, canExecute);
         }
---
+++ 202306231339_ProgressBar/202306231339_ProgressBar/ViewModel/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _202306231339_ProgressBar.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+            //public void OnPropertyChanged([CallerMemberName] string? propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+            //protected void SetProperty<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName] string? name = null)
+            //{
+            //    if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            //    field = value;
+            //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            //}
+
+
+        }
+    }
