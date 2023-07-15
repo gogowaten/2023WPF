2c7a4bfd724b9b801ab78b38baabeb4978cfcf17
waten <gogowaten@yahoo.co.jp>
2023-07-16 08:02:35 +0900

    202307160802

---
+++ 20230716_getPixelTest/20230716_getPixelTest/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20230716_getPixelTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+
+        //DC取得
+        //nullを渡すと画面全体のDCを取得、ウィンドウハンドルを渡すとそのウィンドウのクライアント領域DC
+        //失敗した場合の戻り値はnull
+        //使い終わったらReleaseDC
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        //渡したDCに互換性のあるDC作成
+        //失敗した場合の戻り値はnull
+        //使い終わったらDeleteDC
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        //指定されたDCに関連付けられているデバイスと互換性のあるビットマップを作成
+        //使い終わったらDeleteObject
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int cx, int cy);
+
+        //DCにオブジェクトを指定する、オブジェクトの種類はbitmap、brush、font、pen、Regionなど
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr SelectObject(IntPtr hdc, IntPtr h);
+
+        //画像転送
+        [DllImport("gdi32.dll")]
+        private static extern bool BitBlt(IntPtr hdc, int x, int y, int cx, int cy, IntPtr hdcSrc, int x1, int y1, uint rop);
+        private const int SRCCOPY = 0x00cc0020;
+        private const int SRCINVERT = 0x00660046;
+
+
+        [DllImport("user32.dll")]
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        private static extern bool DeleteObject(IntPtr ho);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
